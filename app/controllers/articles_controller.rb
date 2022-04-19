8732d1607b01436fdc1ee8922f47c6ac56630080
David Mikheev <davidhomemail29@gmail.com>
2022-04-19 10:04:58 +0300

    init commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,8 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: %i[ show edit update destroy ]
   
-  http_basic_authenticate_with name: ["dma", "master"], password: ["nextstep", "rails"], except: [:index, :show]
-
+  http_basic_authenticate_with name: "dma", password: "nextstep", except: [:index, :show]
+  http_basic_authenticate_with name:  "master", password: "rails", except: [:index, :show]
   # GET /articles or /articles.json
   def index
     @articles = Article.all
