29a4ef86effa0bbb95a31974b1dc8bb2d37af3f0
David Mikheev <davidhomemail29@gmail.com>
2022-05-02 22:18:39 +0300

    set webserver

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,8 @@
 class CommentsController < ApplicationController
   before_action :set_article
   
+  http_basic_authenticate_with name:  "master", password: "rails", except: []
+  
   def create 
     @article.comments.create! params.required(:comment).permit(:content)
     redirect_to @article 
